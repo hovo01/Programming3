8679531b1f0183cc761e91853a8787ad87b0e9f8
Your Name <you@example.com>
2018-03-24 13:16:13 +0400

    exanak

--- lesson2/script.js
+++ lesson2/script.js
@@ -1,25 +1,25 @@
- var q0=[0,4];
-var q1=[0,10];
-var q2=[0,8];
-var q3=[0,5];
-var q4=[0,4];
-var q5=[0,6];
-var matrix=[];
-
-   
+var q0 = [0, 4];
+var q1 = [0, 10];
+var q2 = [0, 8];
+var q3 = [0, 5];
+var q4 = [0, 4];
+var q5 = [0, 6];
+var matrix = [];
 
 
-for(var a = 0; a<50; a++){
+var dzmer = true;
+var ashun = true;
+var garun = true;
+var amar = true;
+
+for (var a = 0; a < 50; a++) {
     matrix.push([]);
-     for(var b = 0; b<50; b++){
-        matrix[a].push(Math.floor(Math.random()* Math.floor(6)));
-         
-         
-    
-console.log(matrix);
+    for (var b = 0; b < 50; b++) {
+        matrix[a].push(Math.floor(Math.random() * Math.floor(6)));
 
 
-     }
+
+    }
 }
 
 
@@ -31,16 +31,19 @@ var side = 4;
 var grassArr = [];
 function setup() {
     frameRate(30);
+  textSize(20);
+  textSize(30);
+  textAlign(CENTER);
     createCanvas(matrix[0].length * side, matrix.length * side);
     background('#acacac');
     for (var y = 0; y < matrix.length; ++y) {
         for (var x = 0; x < matrix[y].length; ++x) {
             if (matrix[y][x] == 1) {
-                var gr = new Grass(x, y ,1);
+                var gr = new Grass(x, y, 1);
                 grassArr.push(gr);
             }
             else if (matrix[y][x] == 2) {
-                var xt = new Xotaker(x, y,1);
+                var xt = new Xotaker(x, y, 1);
                 xotakerArr.push(xt);
             }
 
@@ -60,14 +63,27 @@ function setup() {
 
     }
 }
+var winter;
 
 function draw() {
-
+ 
+  background(200);
+  text(frameCount, width / 2, height / 2);
+  console.log(frameCount);
     for (var y = 0; y < matrix.length; y++) {
         for (var x = 0; x < matrix[y].length; x++) {
             if (matrix[y][x] == 1) {
+
                 fill("green");
-                rect(x * side, y * side, side, side);
+                rect(x * side, y * side, side, side); 
+                for (var i = 1; i < 1000; i+=4) {
+                    if (frameCount > 30 *i && frameCount < 80 *i) {
+                    fill("gray");
+                    rect(x * side, y * side, side, side);
+                }
+                }
+                
+
             }
             else if (matrix[y][x] == 0) {
                 fill("#acacac");
@@ -97,7 +113,7 @@ function draw() {
     for (var u in xotakerArr) {
         xotakerArr[u].utel();
         grassArr.splice(u, 1);
-             
+
         if (xotakerArr[u].energy >= 6) {
             xotakerArr[u].bazmanal();
         }
@@ -118,24 +134,24 @@ function draw() {
         if (GishatichArr[g].energy <= -6) {
 
             GishatichArr[g].mernel();
-            GishatichArr.splice(g,1);
+            GishatichArr.splice(g, 1);
         }
     }
-   
+
 
     /////sadaasd
 
 
     for (var t in virusarr) {
         virusarr[t].utel();
-                
+
         for (var k in grassArr) {
             if (grassArr[k].x == virusarr[t].x && grassArr[k].y == virusarr[t].y) {
                 grassArr.splice(k, 1);
                 break;
             }
         }
-         for (var e in GishatichArr) {
+        for (var e in GishatichArr) {
             if (GishatichArr[e].x == virusarr[t].x && GishatichArr[e].y == virusarr[t].y) {
                 GishatichArr.splice(e, 1);
                 break;
@@ -147,16 +163,16 @@ function draw() {
                 break;
             }
         }
-           
+
     }
 
     for (var l in doctorarr) {
         doctorarr[l].bujel();
-        if(doctorarr[l].energy<=-2){
+        if (doctorarr[l].energy <= -2) {
             doctorarr[l].mernel();
         }
-        
+
     }
-    
-    
+
+
 }
